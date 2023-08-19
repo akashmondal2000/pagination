a1406cfe401d5c6b0622142f7bd6f98dab47a881
akashmondal2000 <mondalakash494@gmail.com>
2023-08-19 17:40:44 +0530

    initialize some states

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { useState,useEffect } from "react";
+import axios from "axios";
+import ReactPaginate from "react-paginate";
 
 const App = () => {
+
+  const [offset,setOffset] = useState(0);
+  const [data, setData] = useState([]);
+  const [perPage] = useState(10);
+  const [pageCount, setPageCount] = useState (0);
   return (
     <div className="App">
       <h1>Pagination App</h1>
