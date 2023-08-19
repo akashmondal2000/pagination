ca7fa2411989318532380a58f43b8e4025512cd0
akashmondal2000 <mondalakash494@gmail.com>
2023-08-19 18:12:04 +0530

    add getData function

--- src/App.js
+++ src/App.js
@@ -8,6 +8,22 @@ const App = () => {
   const [data, setData] = useState([]);
   const [perPage] = useState(10);
   const [pageCount, setPageCount] = useState (0);
+
+  const getData = async()=>{
+    const res = await
+    axios.get(`https://jsonplaceholder.typicode.com/photos`)
+    const data = res.data;
+    const slice = data.slice(offset, offset + perPage)
+    const postData = slice.map(pd => {
+      <div key={pd.id}>
+        <p>{pd.title}</p>
+        <img src={pd.thumbnailUrl} alt="" />
+      </div>
+      setData(postData);
+      setPageCount(Math.ceil(data.length / perPage))
+    })
+
+  }
   return (
     <div className="App">
       <h1>Pagination App</h1>
