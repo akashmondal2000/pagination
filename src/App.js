1a62188dc4caa0fb5df3082e44182d79597e2d04
akashmondal2000 <mondalakash494@gmail.com>
2023-08-19 22:22:48 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
-import { useState,useEffect } from "react";
-import axios from "axios";
-import ReactPaginate from "react-paginate";
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import ReactPaginate from 'react-paginate';
+import './App.css'
 
-// https://medium.com/how-to-react/create-pagination-in-react-js-using-react-hooks-c3c582ff5a96
-
-const App = () => {
-
-  const [offset,setOffset] = useState(0);
+function App() {
+  const [offset, setOffset] = useState(0);
   const [data, setData] = useState([]);
   const [perPage] = useState(10);
-  const [pageCount, setPageCount] = useState (0);
+  const [pageCount, setPageCount] = useState(0)
 
-  const getData = async()=>{
-    const res = await
-    axios.get(`https://jsonplaceholder.typicode.com/photos`)
-    const data = res.data;
-    const slice = data.slice(offset, offset + perPage)
-    const postData = slice.map(pd => {
-      <div key={pd.id}>
-        <p>{pd.title}</p>
-        <img src={pd.thumbnailUrl} alt="" />
-      </div>
-      setData(postData);
-      setPageCount(Math.ceil(data.length / perPage))
-    })
+
+  const getData = async() => {
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/photos`)
+      const data = res.data;
+                const slice = data.slice(offset, offset + perPage)
+                const postData = slice.map(pd => <div key={pd.id}>
+                    <p>{pd.title}</p>
+                    <img src={pd.thumbnailUrl} alt=""/>
+                </div>)
+                setData(postData)
+                setPageCount(Math.ceil(data.length / perPage))
   }
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    setOffset(selectedPage + 1)
+};
 
+ useEffect(() => {
+   getData()
+ }, [offset])
 
-  useEffect(()=>{
-    getData();
-  },[offset])
-
-  // Now let's create a method to handle our page click.
-
-  // The method is just updating our offset state by adding 1 on each click using setOffset method.
-  const handlePageClick = (e)=>{
-    const seclectedPage = e.seclected;
-    setOffset(seclectedPage +1);
-  }
   return (
     <div className="App">
-      <h1>Pagination App</h1>
+      {data}
+       <ReactPaginate
+                    previousLabel={"prev"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"}/>
     </div>
   );
 }
